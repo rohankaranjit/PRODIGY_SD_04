3268cce367159104428fa968d30721c4e911baa6
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-06-16 23:37:04 +0545

    Update SD_04_Sudoku_Solver.py

--- SD_04_Sudoku_Solver.py
+++ SD_04_Sudoku_Solver.py
@@ -2,7 +2,6 @@ def is_valid(board, row, col, num):
    # Check if the current row does not contain the same number
     if num in board[row]:
         return False
-
     # Check if the current column does not contain the same number
     for i in range(9):
         if board[i][col] == num:
